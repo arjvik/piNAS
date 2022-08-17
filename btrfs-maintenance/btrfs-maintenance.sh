c0c43d527084c3ecf74c3a967d7cc906bc9c5a52
Arjun Vikram <arjvik@gmail.com>
2022-08-17 13:51:07 -0500

    Add initialization notification to btrfs maintenance script

--- btrfs-maintenance/btrfs-maintenance.sh
+++ btrfs-maintenance/btrfs-maintenance.sh
@@ -8,12 +8,14 @@ WAIT2=7d
 function notify() { echo '$ notify' "$@"; curl -s -F body="${2-$1}" ${2+-F title="${1}"} ${3+-F tag="$(echo "$3" | tr '[:upper:]' '[:lower:]')"} apprise/notify/main; }
 function die() { notify "$@"; exit 1; }
 
+until notify "Initializing BTRFS maintenance process"; do
+	sleep 1;
+done
+
 while true; do
 	sleep "$WAIT1"
 
-	until notify "Starting BTRFS scrub"; do
-		sleep 1;
-	done
+	notify "Starting BTRFS scrub"
 
 	# Assumes that the relevant BTRFS filesystem is the same one storing the docker directory and subvolumes
 	scrub=$(btrfs scrub start -BR / | tee /dev/stderr) || notify "BTRFS Scrub could not complete!" "$scrub" CRITICAL
